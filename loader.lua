a4fb961bc118670b6015a7b624efea6739d84060
DevSourcererScripts <marcouriel776@gmail.com>
2026-03-26 17:33:51 -0600

    Actualizar loader.lua

--- loader.lua
+++ loader.lua
@@ -20,6 +20,20 @@ local MODULES = {
     "module_ui_logs.lua",
 }
 
+for _, name in ipairs(modules) do
+    print("[loader_v4] Loading", name)
+
+    local success, result = pcall(function()
+        return loadstring(readfile(name))()
+    end)
+
+    if success then
+        print("[loader_v4] Loaded:", name)
+    else
+        warn("[loader_v4] Error en", name, result)
+    end
+end
+
 -- =========================
 -- BASE GLOBAL
 -- =========================
